b31d4619b75fa65f27daa70350371fb0d55a7004
Dilja Sigurdardottir <diljas19@ru.is>
2019-09-05 10:25:12 +0000

    Algorithm written for max_int

---
+++ sequence.py
@@ -0,0 +1,17 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+first = 1
+second = 2
+third = 3
+sum_n = 0
+counter = 1
+
+while counter <= n:
+    print(first)
+    print(second)
+    print(third)
+    first = first+second+third
+    second = first+second+third
+    third =  first+second+third
+    counter += 1 -3
+
+
