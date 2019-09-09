2a6e2b81b9dbf945b44ca1ffb2c3a4fd6da8079a
Dilja Sigurdardottir <diljas19@ru.is>
2019-09-09 12:38:19 +0000

    uppfærsla

--- Heimaverkefni/numbers.py
+++ Heimaverkefni/numbers.py
@@ -1,16 +1,16 @@
 for x in range(10,100):
     firstDigit = int(x / 10)                    
     secondDigit = x % 10
-    num = firstDigit + secondDigit
-    if (num ** 2) == x:
+    sumOfDigits = firstDigit + secondDigit
+    if (sumOfDigits ** 2) == x:
         print(x)
 
-count_of_divisors = 0
+countOfDivisors = 0
 
-for x in range(1,100):
-    for y in range(1,(x+1)):
-        if x % y == 0:
-            count_of_divisors += 1
-    if count_of_divisors == 10:
-        print(x)
-    count_of_divisors = 0
+for numberToCheck in range(1,100):
+    for potentialDivisor in range(1,(x+1)):
+        if numberToCheck % potentialDivisor == 0:
+            countOfDivisors += 1
+    if countOfDivisors == 10:
+        print(numberToCheck)
+    countOfDivisors = 0
